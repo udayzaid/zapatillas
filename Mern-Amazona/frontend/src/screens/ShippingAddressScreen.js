483c8466f41e1e19df08e654ff54435d0de5e750
udayzaid <zaidquispe70364819@gmail.com>
2026-09-02 23:17:43 -0400

    Translate shipping address interface

--- Mern-Amazona/frontend/src/screens/ShippingAddressScreen.js
+++ Mern-Amazona/frontend/src/screens/ShippingAddressScreen.js
@@ -60,15 +60,15 @@ export default function ShippingAddressScreen() {
   return (
     <div>
       <Helmet>
-        <title>Shipping Address</title>
+        <title>Dirección de envío</title>
       </Helmet>
 
       <CheckoutSteps step1 step2></CheckoutSteps>
       <div className="container small-container">
-        <h1 className="my-3">Shipping Address</h1>
+        <h1 className="my-3">Dirección de envío</h1>
         <Form onSubmit={submitHandler}>
           <Form.Group className="mb-3" controlId="fullName">
-            <Form.Label>Full Name</Form.Label>
+            <Form.Label>Nombre completo</Form.Label>
             <Form.Control
               value={fullName}
               onChange={(e) => setFullName(e.target.value)}
@@ -76,7 +76,7 @@ export default function ShippingAddressScreen() {
             />
           </Form.Group>
           <Form.Group className="mb-3" controlId="address">
-            <Form.Label>Address</Form.Label>
+            <Form.Label>Dirección</Form.Label>
             <Form.Control
               value={address}
               onChange={(e) => setAddress(e.target.value)}
@@ -84,7 +84,7 @@ export default function ShippingAddressScreen() {
             />
           </Form.Group>
           <Form.Group className="mb-3" controlId="city">
-            <Form.Label>City</Form.Label>
+            <Form.Label>Ciudad</Form.Label>
             <Form.Control
               value={city}
               onChange={(e) => setCity(e.target.value)}
@@ -92,7 +92,7 @@ export default function ShippingAddressScreen() {
             />
           </Form.Group>
           <Form.Group className="mb-3" controlId="postalCode">
-            <Form.Label>Postal Code</Form.Label>
+            <Form.Label>Código postal</Form.Label>
             <Form.Control
               value={postalCode}
               onChange={(e) => setPostalCode(e.target.value)}
@@ -100,7 +100,7 @@ export default function ShippingAddressScreen() {
             />
           </Form.Group>
           <Form.Group className="mb-3" controlId="country">
-            <Form.Label>Country</Form.Label>
+            <Form.Label>País</Form.Label>
             <Form.Control
               value={country}
               onChange={(e) => setCountry(e.target.value)}
@@ -114,21 +114,21 @@ export default function ShippingAddressScreen() {
               variant="light"
               onClick={() => navigate('/map')}
             >
-              Choose Location On Map
+              Elegir ubicación en el mapa
             </Button>
             {shippingAddress.location && shippingAddress.location.lat ? (
               <div>
                 LAT: {shippingAddress.location.lat}
-                LNG:{shippingAddress.location.lng}
+                LNG: {shippingAddress.location.lng}
               </div>
             ) : (
-              <div>No location</div>
+              <div>Sin ubicación seleccionada</div>
             )}
           </div>
 
           <div className="mb-3">
             <Button variant="primary" type="submit">
-              Continue
+              Continuar
             </Button>
           </div>
         </Form>
