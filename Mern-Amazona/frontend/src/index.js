49411a390b4d91de3fcd3c49a3f5a4bd451d16e2
udayzaid <zaidquispe70364819@gmail.com>
2026-09-03 00:44:01 -0400

    Load dropdown layer styles

--- Mern-Amazona/frontend/src/index.js
+++ Mern-Amazona/frontend/src/index.js
@@ -5,6 +5,7 @@ import { PayPalScriptProvider } from '@paypal/react-paypal-js';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import './home-animations.css';
+import './dropdown-layer.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { StoreProvider } from './Store';
